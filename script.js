41ee177f1e548e86cd38ac2215413d899face55c
RamPareek01 <heyram.exe@gmail.com>
2025-11-07 14:51:09 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-  var tl = gsap.timeline();
+function loadingAnimation(){
+      var tl = gsap.timeline();
   tl.from(".line h1", {
     y: 150,
     stagger: 0.25,
     duration: 0.6,
     delay: 0.5,
   });
-  tl.from("#line1-part1",  {
+  tl.from("#line1-part1, .line h2",  {
     opacity: 0,
     onStart: function () {
       var h4timer = document.querySelector("#line1-part1 h4");
@@ -19,16 +20,13 @@
       }, 27);
     },
   });
-  tl.to(".line h2", {
-    animationName: "anime",
-    opacity: 1,
-  });
+
   tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
     delay: 2.6,
   });
-  t1.from("#page1",{
+  tl.from("#page1",{
     delay:0.2,
     y:1600,
     opacity:0,
@@ -37,3 +35,25 @@
   tl.to("#loader",{
     display:"none"
   })
+  tl.from("#nav",{
+    opacity:0
+
+  })
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:150,
+    stagger:0.25,
+
+  })
+}
+loadingAnimation()
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+  gsap.to("#crsr",{
+    left:dets.x,
+    top:dets.y
+  })
+  
+})
+Shery.makeMagnet("#nav h3");
+}
+cursorAnimation()
